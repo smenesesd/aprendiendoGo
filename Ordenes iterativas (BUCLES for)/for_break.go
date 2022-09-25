6d18dc617bc6990f63aeff8083cbf15a19c815a1
Samuel Meneses Díaz <samuelmenesesdiaz@MacBook-Pro-de-Samuel.local>
2022-09-24 23:52:36 -0500

    Creacion de ordenes iterativas bucles for

---
+++ Ordenes iterativas (BUCLES for)/for_break.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for {
+		fmt.Print("Salir? (s/n): ")
+		var c rune
+		fmt.Scanf("%c\n", &c)
+		if c == 'S' || c == 's' {
+			break
+		}
+	}
+	fmt.Println("Adios!")
+}
