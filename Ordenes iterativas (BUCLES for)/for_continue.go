0851b8c186a395715f07e6f8434e71415228395b
Samuel Meneses Díaz <samuelmenesesdiaz@MacBook-Pro-de-Samuel.local>
2022-09-25 14:06:17 -0500

    Creacion de archivo for_continue

---
+++ Ordenes iterativas (BUCLES for)/for_continue.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for {
+		fmt.Print("Salir? (s/n): ")
+		var c rune
+		fmt.Scanf("%c\n", &c)
+		if c == 'N' || c == 'n' {
+			continue
+		}
+		if c == 'S' || c == 's' {
+			break
+		}
+		fmt.Println("Caracter no reconocido")
+	}
+	fmt.Println("Adios!")
+}
