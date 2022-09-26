19a9f4cb233bab21cb6bb7d8a438c74818ea3bef
Samuel Meneses <smenesesd@eafit.edu.co>
2022-09-26 13:30:28 -0500

    Summary(Creacion de README.md y enumeracion de la carpeta)
    Creacion del README.md y enumeracion de la carpeta 2.12

---
+++ 2.12 Salida De Datos/SalidaEstandarDatos.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	cosa := "deposito"
+	x := 36
+	y := 84
+	fmt.Printf("Coordenadas de %v: (%v, %v)\n", cosa, x, y)
+}
---
+++ 2.12 Salida De Datos/SalidaEstandarDeDat.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	cosa := "Deposito"
+	x := 36
+	y := 84
+
+	fmt.Printf("Coordenadas de %v: (%v, %v)", cosa, x, y)
+}
